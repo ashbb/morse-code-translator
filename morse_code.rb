389fcf7cfcbefe8b74c59e35e2ee1fceafbed74e
Willian Molinari <willian.molinari@gmail.com>
2009-03-27 03:54:11 -0300

    First commit of incredible morse code translator! =P

---
+++ morse_code.rb
@@ -0,0 +1,60 @@
+HASH_MORSE = {
+  '.-' => 'a',
+  '-..' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '8',
+  '-----' => '0',
+  '/' => ' ',
+}
+
+class ConvertToMorse
+  def get_translated(string_morse)
+    @translated_string = ""
+    translate(string_morse)
+  end
+
+  private
+  def translate(string_morse)
+    array_morse = string_morse.split()
+    array_morse.each{|morse| @translated_string += HASH_MORSE[morse] unless HASH_MORSE[morse].nil? }
+    puts @translated_string
+  end
+end
+
+morse = ConvertToMorse.new
+
+morse.get_translated("-- --- .-. ... . / -.-. --- -.. . / - .... . / -. . .-- / -.. . .- -.. / .-.. .- -. --. ..- .- --. . .-.-.-")
+morse.get_translated("--- -- --.  .. ...  -- --- .-. ... .  -.-. --- -..")
+morse.get_translated("- .-- . . - ... / .- .-. . / . ...- . -. / ... .... --- .-. - . .-. / .-- .. - .... / -- --- .-. ... . / -.-. --- -..")
+morse.get_translated("-- --- .-. ... .   -.-. --- -.. .   .. ...   -... .- -..   ..-. --- .-.   - .-- . . - .. -. --")
