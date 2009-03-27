0cb906b7c91bb50fa195091805e2740b7ea68bf3
Willian Molinari <willian.molinari@gmail.com>
2009-03-27 17:01:50 -0300

    Changing the method

--- morse_code.rb
+++ morse_code.rb
@@ -35,20 +35,20 @@ HASH_MORSE = {
   '---..' => '8',
   '----.' => '8',
   '-----' => '0',
-  '/' => ' ',
+  '/' => ' ' # Mapping foward bar to be a space
 }
 
 class ConvertFromMorse
   def get_translated(string_morse)
     @translated_string = ""
-    translate(string_morse)
+    puts translate(string_morse)
   end
 
   private
   def translate(string_morse)
     array_morse = string_morse.split()
     array_morse.each{|morse| @translated_string += HASH_MORSE[morse] unless HASH_MORSE[morse].nil? }
-    puts @translated_string
+    @translated_string
   end
 end
 
@@ -58,3 +58,4 @@ morse.get_translated("-- --- .-. ... . / -.-. --- -.. . / - .... . / -. . .-- / 
 morse.get_translated("--- -- --.  .. ...  -- --- .-. ... .  -.-. --- -..")
 morse.get_translated("- .-- . . - ... / .- .-. . / . ...- . -. / ... .... --- .-. - . .-. / .-- .. - .... / -- --- .-. ... . / -.-. --- -..")
 morse.get_translated("-- --- .-. ... .   -.-. --- -.. .   .. ...   -... .- -..   ..-. --- .-.   - .-- . . - .. -. --")
+morse.get_translated(".. - /  .. ... /  .- /  .--. .-.. . .- ... ..- .-. . /  - --- /  -- . . - /  -.-- --- ..- /  .- ... /  .-- . .-.. .-.")
